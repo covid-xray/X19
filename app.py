65f313636f4219f2bd4357b564146da277f1f638
binti-p <padaliya.binti@gmail.com>
2021-03-01 19:15:52 +0530

    fixed error

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request, session, redirect, url_for, flash, re
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 import os
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
